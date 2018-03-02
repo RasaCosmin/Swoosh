4f1a24d319b3cac57c19db47d4a6e1c82fc1ebb0
Rasa Cosmin <rasa.cosmin@gmail.com>
2018-03-02 10:51:07 +0200

    add buttons function and send on intent

--- app/src/main/java/com/rasik/swoosh/SkillActivity.kt
+++ app/src/main/java/com/rasik/swoosh/SkillActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 
 class SkillActivity : BaseActivity() {
 
+    var selectedLeague = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        selectedLeague = intent.getStringExtra(EXTRA_LEAGUE)
+        println(selectedLeague)
     }
 }
